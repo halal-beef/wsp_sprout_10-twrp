6a501880a83a5ae48bbd08cee1f30f504af0c38a
smallpp420 <78730004+SmallPP420@users.noreply.github.com>
2021-11-28 20:50:14 +0000

    Update omni_WSP_sprout.mk

--- omni_WSP_sprout.mk
+++ omni_WSP_sprout.mk
@@ -17,10 +17,10 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Inherit from WSP_sprout device
 #$(call inherit-product, device/hmd/WSP_sprout/device.mk)
